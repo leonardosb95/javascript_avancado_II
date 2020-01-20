5f800b53f5671e6a859ddce1795c788b9327fb57
leonardosb95 <leonardosantosbispo@gmail.com>
2020-01-20 09:37:03 -0300

    Projeto Factory

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,43 +7,19 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        let self=this;
+        this._listaNegociacoes = ProxyFactory.create(
 
-        this._listaNegociacoes=new Proxy(new ListaNegociacoes(), {
-
-            get(target, prop, receiver) {
-                if (['adiciona', 'esvazia'].includes(prop) && typeof (target[prop]) == typeof (Function)) {//Verifica se a propriedade do Objeto e adiciona ou esvazia
-
-                    return function () {
-
-                        console.log(`interceptando ${prop}`);
-                        Reflect.apply(target[prop], target, arguments);//retorna o valor da propriedade, a referência ao Objeto e coloca os argumentos da propriedade
-                        self._negociacaoView.update(target);//para referência ao NegociacaoController
-
-                    }
-                }
-
-                return Reflect.get(target, prop, receiver);
-            }
-
-
-            // set: function(target, prop, value,receiver) {
-
-            //     console.log(`Antigo valor: ${target[prop]} /Novo valor:${value}`);
-            //     return Reflect.set(target, prop, value, receiver);
-
-            // }
-        });
-
-
-        // this._listaNegociacoes = new ListaNegociacoes(model=>// O model é a instância da função que vai ser chamada
-        //     this._negociacaoView.update(model));// Quando ele chamar o evazia ou adiciona, ele dispara o update
-
+            new ListaNegociacoes(),
+            ['adiciona', 'esvazia'], model =>
+            this._negociacaoView.update(model));
 
         this._negociacaoView = new NegociacoesView($("#negociacaoView"));
         this._negociacaoView.update(this._listaNegociacoes);
 
-        this._mensagem = new Mensagem();
+        this._mensagem = ProxyFactory.create(
+            new Mensagem(), ['texto'], model =>
+            this._mensagemView.update(model));
+
         this._mensagemView = new MensagemView($("#MensagemView"));
         this._mensagemView.update(this._mensagem);
 
@@ -56,10 +32,9 @@ class NegociacaoController {
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._limpaFormulario();
         this._mensagem.texto = 'Negociacao adicionada com sucesso';
-        this._mensagemView.update(this._mensagem);
         console.log(this._listaNegociacoes);
     }
-    
+
     apaga() {
 
         this._listaNegociacoes.esvazia();
@@ -67,7 +42,7 @@ class NegociacaoController {
         console.log(this._mensagem);
     }
     _criaNegociacao() {
-        
+
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
